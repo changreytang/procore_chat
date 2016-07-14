2365803c19fb28bdbc937d129e86d3ca4834bc21
Vivek Patel <vivek.patel@procore.com>
2016-07-14 13:47:19 -0700

    Added users switch

--- client/app/bundles/Chat/src/reducers.js
+++ client/app/bundles/Chat/src/reducers.js
@@ -1,62 +1,72 @@
 const initialState = {
-  channels: [],
+	channels: [],
 }
 
 const reducer = (state = initialState, action) => {
-  const {
-    type,
-    messagingClient,
-    channels,
-    uniqueName,
-    message,
-    messages,
-  } = action
-  switch (type) {
-    case 'CREATE_MESSAGING_CLIENT':
-      return { ...state, messagingClient }
-    case 'GET_CHANNELS':
-      return { ...state, channels }
-    case 'GET_MESSAGES':
-      return {
-        ...state,
-        channels: state.channels.map(channel => {
-          if (channel.uniqueName === uniqueName) {
-            return { ...channel, messages }
-          } else {
-            return channel
-          }
-        }),
-      }
-    case 'MESSAGE_ADDED':
-      return {
-        ...state,
-        channels: state.channels.map(channel => {
-          if (channel.uniqueName === uniqueName) {
-            const messages = channel.messages.concat(message)
-            return { ...channel, messages }
-          } else {
-            return channel
-          }
-        }),
-      }
-    case 'ACTIVATE_CHANNEL':
-      return {
-        ...state,
-        channels: state.channels.map(channel => {
-          if (channel.uniqueName === uniqueName) {
-            return { ...channel, active: true }
-          } else {
-            return channel
-          }
-        }),
-      }
-    case 'SEND_MESSAGE':
-      state.messagingClient.getChannelByUniqueName(uniqueName)
-        .then(channel => channel.sendMessage(message))
-      return state
-    default:
-      return state
-  }
+	const {
+		type,
+		messagingClient,
+		channels,
+		users,
+		uniqueName,
+		message,
+		messages,
+	} = action
+	switch (type) {
+		case 'CREATE_MESSAGING_CLIENT':
+			return {...state, messagingClient
+			}
+		case 'GET_CHANNELS':
+			return {...state, channels
+			}
+		case 'GET_USERS':
+			return {...state, users
+			}
+		case 'GET_MESSAGES':
+			return {
+					...state,
+				channels: state.channels.map(channel => {
+					if (channel.uniqueName === uniqueName) {
+						return {...channel, messages
+						}
+					} else {
+						return channel
+					}
+				}),
+			}
+		case 'MESSAGE_ADDED':
+			return {
+					...state,
+				channels: state.channels.map(channel => {
+					if (channel.uniqueName === uniqueName) {
+						const messages = channel.messages.concat(message)
+						return {...channel, messages
+						}
+					} else {
+						return channel
+					}
+				}),
+			}
+		case 'ACTIVATE_CHANNEL':
+			return {
+					...state,
+				channels: state.channels.map(channel => {
+					if (channel.uniqueName === uniqueName) {
+						return {...channel, active: true
+						}
+					} else {
+						return channel
+					}
+				}),
+			}
+		case 'SEND_MESSAGE':
+			state.messagingClient.getChannelByUniqueName(uniqueName)
+				.then(channel => channel.sendMessage(message))
+			return state
+		default:
+			return state
+	}
 }
 
-export default reducer
+export
+default reducer
