547f820eaa99d1aefaa5e75fd3ffc6186155fd00
James Vaughan <james@jamesbvaughan.com>
2016-07-15 09:21:32 -0700

    updated package.json

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,9 +2,10 @@ class ChatsController < ApplicationController
   before_action :logged_in_user
 
 	def home
+    identity = current_user.name
     token = Twilio::Util::AccessToken.new "AC6e8a6ac0cde02b70cf13757258df57fc",
         "SK900b984c5d76c02721eed8bbf2a98fc6",
-        "BCOR2EW40sDQQv9N3N5caVNM1tve1ebM", 3600, current_user.name
+        "BCOR2EW40sDQQv9N3N5caVNM1tve1ebM", 3600, identity
 
     # Create IP Messaging grant for our token
     grant = Twilio::Util::AccessToken::IpMessagingGrant.new
@@ -15,5 +16,5 @@ class ChatsController < ApplicationController
     @token = token.to_jwt
     @users = User.all
   end
-  
+
 end
