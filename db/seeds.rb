70669005a5f0cf181e35bb94b07a82e9a0f7fd44
James Vaughan <james@jamesbvaughan.com>
2016-07-20 15:01:23 -0700

    prettified seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!([{ name: "James Vaughan", email: "jamesvaughan@gmail.com"}, { name: "Vivek Patel", email: "vivekpatel@gmail.com"},
-				{ name: "Rey Tang", email: "reytang@gmail.com"}, { name: "Sam Lee", email: "samlee@gmail.com"},
-				{ name: "Rose Chang", email: "rosechang@gmail.com"}, { name: "Wes Han", email: "weshan@gmail.com"}])
+User.create!([
+  { name: "James Vaughan", email: "jamesvaughan@gmail.com" },
+  { name: "Vivek Patel",   email: "vivekpatel@gmail.com"   },
+	{ name: "Rey Tang",      email: "reytang@gmail.com"      },
+  { name: "Sam Lee",       email: "samlee@gmail.com"       },
+	{ name: "Rose Chang",    email: "rosechang@gmail.com"    },
+  { name: "Wes Han",       email: "weshan@gmail.com"       }
+])
