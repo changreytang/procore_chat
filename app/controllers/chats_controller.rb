96b53036b99bc6ed79ffeadb30695b221e94e9cd
changreytang <tang.changrey@gmail.com>
2016-07-20 14:05:12 -0700

    reset keys

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -4,21 +4,21 @@ class ChatsController < ApplicationController
     before_action :logged_in_user
 
     def home
-        identity = current_user.name
+        identity = current_user.id
         token = Twilio::Util::AccessToken.new "AC6e8a6ac0cde02b70cf13757258df57fc",
             "SK900b984c5d76c02721eed8bbf2a98fc6",
             "BCOR2EW40sDQQv9N3N5caVNM1tve1ebM", 3600, identity
 
         # Create IP Messaging grant for our token
         grant = Twilio::Util::AccessToken::IpMessagingGrant.new
-        grant.service_sid = "IS06c3efca34a849cdaf9ee31cfa99b816"
+        grant.service_sid = "IS63900aeb7f6a40478fada67771a95dc9"
         grant.endpoint_id = "procore_chat:#{identity}:browser"
         token.add_grant grant
 
         @token = token.to_jwt
         @users = User.all
 
-        cmd = "curl -X POST https://ip-messaging.twilio.com/v1/Services/IS06c3efca34a849cdaf9ee31cfa99b816 \
+        cmd = "curl -X POST https://ip-messaging.twilio.com/v1/Services/IS63900aeb7f6a40478fada67771a95dc9 \
                -d 'ReachabilityEnabled=true' \
                -u 'AC6e8a6ac0cde02b70cf13757258df57fc:4cd11afae4208bf3beead39b6c23a389'"
 
