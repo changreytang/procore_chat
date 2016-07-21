616afb72424eaa12099d8c0725e4dd4555d7711f
James Vaughan <james@jamesbvaughan.com>
2016-07-21 16:49:03 -0700

    whitespace changes

--- client/app/reducers.js
+++ client/app/reducers.js
@@ -1,50 +1,50 @@
 import { generateUniqueChannelName } from './utils'
 
 export default (state, action) => {
-	let channels, users
-	const { input, type, currentUser, uniqueName, name, channel, online,
-		      message, messages, messagingClient, identity, userActivated, unread } = action
+  let channels, users
+  const { input, type, currentUser, uniqueName, name, channel, online,
+          message, messages, messagingClient, identity, userActivated, unread } = action
 
-	switch (type) {
-		case 'SET_MESSAGING_CLIENT':
-			return { ...state, messagingClient }
-		case 'GET_CURRENT_USER':
-			return { ...state, currentUser }
-		case 'GET_USERS':
+  switch (type) {
+    case 'SET_MESSAGING_CLIENT':
+      return { ...state, messagingClient }
+    case 'GET_CURRENT_USER':
+      return { ...state, currentUser }
+    case 'GET_USERS':
       users = action.users
-			return { ...state, users }
-		case 'ACTIVATE_CHANNEL':
+      return { ...state, users }
+    case 'ACTIVATE_CHANNEL':
       const newChannel = {...channel, name, expanded: userActivated }
-			if((window.innerWidth - 450) > document.getElementById('channels').offsetWidth)
-				channels = state.channels.concat(newChannel)
-			else {
-				channels = state.channels.slice(0, state.channels.length - 1).concat(newChannel)
-			}
-			return { ...state, channels }
-		case 'CLOSE_CHANNEL':
-			channels = state.channels.filter(c => c.uniqueName != uniqueName)
-			return { ...state, channels }
+      if((window.innerWidth - 450) > document.getElementById('channels').offsetWidth)
+        channels = state.channels.concat(newChannel)
+      else {
+        channels = state.channels.slice(0, state.channels.length - 1).concat(newChannel)
+      }
+      return { ...state, channels }
+    case 'CLOSE_CHANNEL':
+      channels = state.channels.filter(c => c.uniqueName != uniqueName)
+      return { ...state, channels }
     case 'UPDATE_UNREAD':
-			channels = state.channels.map(channel => {
-				if (channel.uniqueName === uniqueName) {
-					return { ...channel, unread }
-				} else {
-					return channel
-				}
-			})
-			return { ...state, channels }
+      channels = state.channels.map(channel => {
+        if (channel.uniqueName === uniqueName) {
+          return { ...channel, unread }
+        } else {
+          return channel
+        }
+      })
+      return { ...state, channels }
     case 'TOGGLE_EXPAND':
-			channels = state.channels.map(channel => {
-				if (channel.uniqueName === uniqueName) {
+      channels = state.channels.map(channel => {
+        if (channel.uniqueName === uniqueName) {
           const expanded = !channel.expanded
-					return { ...channel, expanded }
-				} else {
-					return channel
-				}
-			})
-			return { ...state, channels }
-		case 'SEARCH_USERS':
-			return {...state, userListSearchQuery: input}
+          return { ...channel, expanded }
+        } else {
+          return channel
+        }
+      })
+      return { ...state, channels }
+    case 'SEARCH_USERS':
+      return {...state, userListSearchQuery: input}
     case 'UPDATE_STATUS':
       users = state.users.map(user => {
         if (user.id.toString() === identity) {
@@ -54,26 +54,26 @@ export default (state, action) => {
         }
       })
       return { ...state, users }
-		case 'GET_MESSAGES':
-			channels = state.channels.map(channel => {
-				if (channel.uniqueName === uniqueName) {
-					return { ...channel, messages }
-				} else {
-					return channel
-				}
-			})
-			return { ...state, channels }
-		case 'MESSAGE_ADDED':
-			channels = state.channels.map(channel => {
-				if (channel.uniqueName === uniqueName) {
-					const messages = channel.messages.concat(message)
-					return {...channel, messages }
-				} else {
-					return channel
-				}
-			})
-			return { ...state, channels }
-		default:
-			return state
-	}
+    case 'GET_MESSAGES':
+      channels = state.channels.map(channel => {
+        if (channel.uniqueName === uniqueName) {
+          return { ...channel, messages }
+        } else {
+          return channel
+        }
+      })
+      return { ...state, channels }
+    case 'MESSAGE_ADDED':
+      channels = state.channels.map(channel => {
+        if (channel.uniqueName === uniqueName) {
+          const messages = channel.messages.concat(message)
+          return {...channel, messages }
+        } else {
+          return channel
+        }
+      })
+      return { ...state, channels }
+    default:
+      return state
+  }
 }
